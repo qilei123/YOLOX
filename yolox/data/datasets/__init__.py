8509cddf9aca6aa547a24d1f2a1652ddd8fa0be0
qilei123 <qileimail123@gmail.com>
2021-07-31 00:22:32 -0400

    todo tests

--- yolox/data/datasets/__init__.py
+++ yolox/data/datasets/__init__.py
@@ -2,9 +2,9 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) Megvii, Inc. and its affiliates.
 
-from .coco import COCODataset
+from .coco import COCODataset, ErosiveUlcer
 from .coco_classes import COCO_CLASSES
 from .datasets_wrapper import ConcatDataset, Dataset, MixConcatDataset
 from .mosaicdetection import MosaicDetection
 from .voc import VOCDetection
-from .erosive_ulcer_mix import ErosiveUlcer
+#from .erosive_ulcer_mix import ErosiveUlcer
