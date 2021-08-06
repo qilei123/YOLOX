e14a823038088c07844d6983f9271d2e9425d7a9
qilei123 <qileimail123@gmail.com>
2021-08-06 15:01:26 -0400

    todo tests

--- tools/eval_erosive_ulcer.py
+++ tools/eval_erosive_ulcer.py
@@ -17,7 +17,7 @@ colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 def eval_erosive_ulcer(dataset_dir,confg_name = "yolox_x_erosive_ulcer_mix_512"):
     exp_file = "exps/erosive_ulcer_mix/"+confg_name+".py"
     exp = get_exp(exp_file, None)
-    exp.test_conf = 0.01
+    exp.test_conf = 0.05
     exp.nmsthre = 0.1
     model = exp.get_model()
     model.cuda()
