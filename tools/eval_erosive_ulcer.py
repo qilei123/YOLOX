17a1351bb61504b3d227aea53022d0ac55989cd3
qilei123 <qileimail123@gmail.com>
2021-08-06 18:40:11 -0400

    todo tests

--- tools/eval_erosive_ulcer.py
+++ tools/eval_erosive_ulcer.py
@@ -26,7 +26,7 @@ def anns2gtboxes(gtanns,categories=[1,2]):
             gtboxes.append(xyxy)
     return gtboxes
 
-def get_eval_outputs(output,ratio=1):
+def get_eval_outputs(output,ratio):
     eval_outputs = []
     
     if output is None:
@@ -69,11 +69,8 @@ def eval_erosive_ulcer(dataset_dir,confg_name = "yolox_x_erosive_ulcer_mix_512")
 
     coco_instance = COCO(os.path.join(dataset_dir,"annotations","test_mix.json"))
     coco_imgs = coco_instance.imgs
-    count_img = 0
+
     for img_id in coco_imgs:
-        count_img+=1
-        if count_img>100:
-            break
         img_name = coco_imgs[img_id]["file_name"]
         img_dir = os.path.join(dataset_dir,"images",img_name)
         #if "00b04d25-1db7-4223-8180-8f3df2c46d05" in img_name:
@@ -84,16 +81,23 @@ def eval_erosive_ulcer(dataset_dir,confg_name = "yolox_x_erosive_ulcer_mix_512")
 
             outputs, img_info = predictor.inference(img_dir)
             #print(outputs)
-            eval_outputs = get_eval_outputs(outputs[0])
+            eval_outputs = get_eval_outputs(outputs[0],img_info["ratio"])
             #print(eval_outputs)
             #eval_m.eval_add_result(gtboxes, filed_boxes,image=image,image_name=coco_instance.imgs[img_id]["file_name"])
             eval_m.eval_add_result(gtboxes, eval_outputs)
-            '''
-            result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
             
-            annIds = coco_instance.getAnnIds(imgIds=coco_imgs[img_id]['id'])
-            anns = coco_instance.loadAnns(annIds)
-            for ann in anns:
+            #result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
+            result_image = predictor.visual(None, img_info, predictor.confthre)
+            #annIds = coco_instance.getAnnIds(imgIds=coco_imgs[img_id]['id'])
+            #anns = coco_instance.loadAnns(annIds)
+
+            for pre_box in eval_outputs:
+                cv2.putText(result_image, Erosive_Ulcer[int(pre_box[4])-1], (int(pre_box[0]), int(
+                    pre_box[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 1, cv2.LINE_AA)
+                cv2.rectangle(result_image, (int(pre_box[0]), int(pre_box[1])), (int(pre_box[2]),
+                                                        int(pre_box[3])), (0,255,0), 1)                
+
+            for ann in gtanns:
                 [x, y, w, h] = ann['bbox']
                 # print(ann['category_id'])
                 cv2.putText(result_image, Erosive_Ulcer[ann['category_id']-1], (int(x), int(
@@ -102,7 +106,7 @@ def eval_erosive_ulcer(dataset_dir,confg_name = "yolox_x_erosive_ulcer_mix_512")
                                                         int(y+h)), (0,255,0), 1)
 
             cv2.imwrite("YOLOX_outputs/"+confg_name+"/vis_results/"+img_name,result_image)
-            '''
+            
     category = eval_m.classes
     evaluation = eval_m.get_result()
     for key in evaluation:
