221987fcb86620c9a28f0395c1d94f386f9ad7df
qilei123 <qileimail123@gmail.com>
2021-08-13 16:54:12 -0400

    todo tests

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 img = cv2.imread('/data2/qilei_chen/DATA/trans_drone/videos/results1/test.jpg') 
 rows,cols = img.shape[:2] 
 #第一个参数旋转中心，第二个参数旋转角度，第三个参数：缩放比例 
-M = cv2.getRotationMatrix2D(angle=0, center=(cols/2, rows/2), scale=0.8) 
+M = cv2.getRotationMatrix2D(angle=5, center=(cols/2, rows/2), scale=0.8) 
 #第三个参数：变换后的图像大小 
 res = cv2.warpAffine(img,M,(cols,rows)) 
 
