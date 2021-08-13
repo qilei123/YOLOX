4f19cbde54c7b019c19e8f593ed9c156ce58c276
qilei123 <qileimail123@gmail.com>
2021-08-13 16:06:10 -0400

    todo tests

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cv2 
 import matplotlib.pyplot as plt 
  
-img = cv2.imread('flower.jpg') 
+img = cv2.imread('/data2/qilei_chen/DATA/trans_drone/videos/results1/test.jpg') 
 rows,cols = img.shape[:2] 
 #第一个参数旋转中心，第二个参数旋转角度，第三个参数：缩放比例 
 M = cv2.getRotationMatrix2D((cols/2,rows/2),45,1) 
