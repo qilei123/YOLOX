7f27cf56240c4073612b7b2673edd639caac2633
qilei123 <qileimail123@gmail.com>
2021-08-23 17:10:08 -0400

    todo tests

--- exps/erosive_ulcer_mix/yolox_x_erosive_ulcer_mix_512.py
+++ exps/erosive_ulcer_mix/yolox_x_erosive_ulcer_mix_512.py
@@ -38,7 +38,105 @@ class Exp(MyExp):
         self.eval_interval = 1
         self.print_interval = 40
 
-    def get_data_loader(self, batch_size, is_distributed, no_aug=False):
+
+    def get_data_loader(
+        self, batch_size, is_distributed, no_aug=False, cache_img=False):
+        from yolox.data import (
+            COCODataset,
+            TrainTransform,
+            YoloBatchSampler,
+            DataLoader,
+            InfiniteSampler,
+            MosaicDetection,
+            worker_init_reset_seed,
+        )
+        from yolox.utils import (
+            wait_for_the_master,
+            get_local_rank,
+        )
+
+        local_rank = get_local_rank()
+
+        with wait_for_the_master(local_rank):
+            dataset = COCODataset(
+                data_dir="/data1/qilei_chen/DEVELOPMENTS/YOLOX/datasets/gastric_object_detection",
+                json_file=self.train_ann,
+                img_size=self.input_size,
+                preproc=TrainTransform(max_labels=50),
+                cache=cache_img,
+            )
+
+        dataset = MosaicDetection(
+            dataset,
+            mosaic=not no_aug,
+            img_size=self.input_size,
+            preproc=TrainTransform(max_labels=120),
+            degrees=self.degrees,
+            translate=self.translate,
+            scale=self.scale,
+            #mscale= self.scale,
+            shear=self.shear,
+            perspective=self.perspective,
+            enable_mixup=self.enable_mixup,
+            mosaic_prob=self.mosaic_prob,
+            mixup_prob=self.mixup_prob,
+        )
+
+        self.dataset = dataset
+
+        if is_distributed:
+            batch_size = batch_size // dist.get_world_size()
+
+        sampler = InfiniteSampler(len(self.dataset), seed=self.seed if self.seed else 0)
+
+        batch_sampler = YoloBatchSampler(
+            sampler=sampler,
+            batch_size=batch_size,
+            drop_last=False,
+            mosaic=not no_aug,
+        )
+
+        dataloader_kwargs = {"num_workers": self.data_num_workers, "pin_memory": True}
+        dataloader_kwargs["batch_sampler"] = batch_sampler
+
+        # Make sure each process has different random seed, especially for 'fork' method.
+        # Check https://github.com/pytorch/pytorch/issues/63311 for more details.
+        dataloader_kwargs["worker_init_fn"] = worker_init_reset_seed
+
+        train_loader = DataLoader(self.dataset, **dataloader_kwargs)
+
+        return train_loader
+
+    def get_eval_loader(self, batch_size, is_distributed, testdev=False, legacy=False):
+        from yolox.data import COCODataset, ValTransform
+
+        valdataset = COCODataset(
+            data_dir="/data1/qilei_chen/DEVELOPMENTS/YOLOX/datasets/gastric_object_detection",
+            json_file=self.val_ann,
+            name="images",
+            img_size=self.test_size,
+            preproc=ValTransform(legacy=legacy),
+        )
+
+        if is_distributed:
+            batch_size = batch_size // dist.get_world_size()
+            sampler = torch.utils.data.distributed.DistributedSampler(
+                valdataset, shuffle=False
+            )
+        else:
+            sampler = torch.utils.data.SequentialSampler(valdataset)
+
+        dataloader_kwargs = {
+            "num_workers": self.data_num_workers,
+            "pin_memory": True,
+            "sampler": sampler,
+        }
+        dataloader_kwargs["batch_size"] = batch_size
+        val_loader = torch.utils.data.DataLoader(valdataset, **dataloader_kwargs)
+
+        return val_loader
+    '''
+    def get_data_loader_(self, batch_size, is_distributed, no_aug=False):
         from yolox.data import (
             ErosiveUlcer,
             TrainTransform,
@@ -99,8 +197,9 @@ class Exp(MyExp):
         train_loader = DataLoader(self.dataset, **dataloader_kwargs)
 
         return train_loader
-
-    def get_eval_loader(self, batch_size, is_distributed, testdev=False):
+    
+    
+    def get_eval_loader_(self, batch_size, is_distributed, testdev=False):
         from yolox.data import ErosiveUlcer, ValTransform
 
         valdataset = ErosiveUlcer(
@@ -130,3 +229,4 @@ class Exp(MyExp):
         val_loader = torch.utils.data.DataLoader(valdataset, **dataloader_kwargs)
 
         return val_loader
+    '''
