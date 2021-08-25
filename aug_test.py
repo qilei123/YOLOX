944c1cad0579e8b4dceccb3bc2aa53261b8527f5
qilei123 <qileimail123@gmail.com>
2021-08-25 18:41:39 -0400

    todo tests

--- aug_test.py
+++ aug_test.py
@@ -3,6 +3,6 @@ import cv2
 
 img = cv2.imread("/data1/qilei_chen/DEVELOPMENTS/YOLOX/datasets/gastric_object_detection/images/0_03_00.700000.jpg")
 
-res_img = random_perspective(img)
+res_img = random_perspective(img,scale=[0.1,1.5])
 
 cv2.imwrite("datasets/test.jpg",res_img)
