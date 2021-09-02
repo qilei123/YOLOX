7bbc8821f4d819a14e0152eff240ad5e37a085c5
qilei123 <qileimail123@gmail.com>
2021-09-02 10:18:15 -0400

    todo tests

--- exps/erosive_ulcer_mix/yolox_x_erosive_ulcer_mix_512.py
+++ exps/erosive_ulcer_mix/yolox_x_erosive_ulcer_mix_512.py
@@ -26,12 +26,34 @@ class Exp(MyExp):
         self.data_num_workers = 1
         self.input_size = (512, 512)
 
-        self.max_epoch = 150
-
+        self.max_epoch = 200
+        self.eval_interval = 1
+        self.print_interval = 50
+        self.degrees =20.0
+        
         self.test_size = (512, 512)
         self.test_conf = 0.01
         self.nmsthre = 0.1
+        self.use_l1 = True
 
+    def get_model(self):
+        from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
+
+        def init_yolo(M):
+            for m in M.modules():
+                if isinstance(m, nn.BatchNorm2d):
+                    m.eps = 1e-3
+                    m.momentum = 0.03
+
+        if getattr(self, "model", None) is None:
+            in_channels = [256, 512, 1024]
+            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels)
+            head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels,use_l1=self.use_l1)
+            self.model = YOLOX(backbone, head)
+
+        self.model.apply(init_yolo)
+        self.model.head.initialize_biases(1e-2)
+        return self.model
     def get_data_loader(self, batch_size, is_distributed, no_aug=False):
         from yolox.data import (
             ErosiveUlcer,
