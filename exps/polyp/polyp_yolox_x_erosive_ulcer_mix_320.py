bfe499f4a8e956aa91f5fb7851074c098fe28dca
qilei123 <qileimail123@gmail.com>
2021-09-24 01:20:48 -0400

    todo tests

--- exps/polyp/polyp_yolox_x_erosive_ulcer_mix_320.py
+++ exps/polyp/polyp_yolox_x_erosive_ulcer_mix_320.py
@@ -28,7 +28,7 @@ class Exp(MyExp):
 
         self.max_epoch = 300
         self.eval_interval = 1
-        self.print_interval = 200
+        self.print_interval = 400
         self.degrees =20.0 #0.7_15 484_336
 
         self.test_size = self.input_size 
--- exps/polyp/polyp_yolox_x_erosive_ulcer_mix_412.py
+++ exps/polyp/polyp_yolox_x_erosive_ulcer_mix_412.py
@@ -27,7 +27,7 @@ class Exp(MyExp):
         self.input_size = (412, 412)
         self.random_size = (20, 30)
 
-        self.max_epoch = 300
+        self.max_epoch = 400
         self.eval_interval = 1
         self.print_interval = 200
         self.degrees =20.0 #0.7_15 484_336
