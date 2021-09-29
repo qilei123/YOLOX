04c9aa36f4bed1a7876ddcd0657e8fbe19bc678a
qilei123 <qileimail123@gmail.com>
2021-09-29 10:52:09 -0400

    todo tests

--- exps/polyp/polyp_yolox_x_erosive_ulcer_mix_320_adjust.py
+++ exps/polyp/polyp_yolox_x_erosive_ulcer_mix_320_adjust.py
@@ -34,7 +34,7 @@ class Exp(MyExp):
 
         self.test_size = self.input_size 
         self.test_conf = 0.01
-        self.nmsthre = 0.4
+        self.nmsthre = 0.1
 
         self.use_l1 = True
 
