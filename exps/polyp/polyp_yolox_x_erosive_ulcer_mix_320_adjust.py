43e490d62a6f23109509e77f3bd0e8ed9d461ff9
qilei123 <qileimail123@gmail.com>
2021-09-29 16:10:48 -0400

    todo tests

--- exps/polyp/polyp_yolox_x_erosive_ulcer_mix_320_adjust.py
+++ exps/polyp/polyp_yolox_x_erosive_ulcer_mix_320_adjust.py
@@ -25,11 +25,11 @@ class Exp(MyExp):
 
         self.data_num_workers = 1
         self.input_size = (320, 320)
-        self.random_size = (5, 25)
+        #self.random_size = (5, 25)
 
         self.max_epoch = 300
         self.eval_interval = 1
-        self.print_interval = 400
+        self.print_interval = 200
         self.degrees =20.0 
 
         self.test_size = self.input_size 
