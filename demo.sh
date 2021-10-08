9ce0023f68ef661b6ff5284b1edc0750e686f906
qilei123 <qileimail123@gmail.com>
2021-10-08 13:46:18 -0400

    todo tests

--- demo.sh
+++ demo.sh
@@ -1,3 +1,7 @@
-python tools/demo.py image -f exps/erosive_ulcer_mix/yolox_x_erosive_ulcer_mix.py -c YOLOX_outputs/yolox_x_erosive_ulcer_mix/best_ckpt.pth.tar --path ./datasets/gastric_object_detection/images/07e40950-e41d-4fb8-9a36-3dc15b63bb75.jpg --conf 0.2 --nms 0.1 --tsize 640 --save_result --device gpu
-
+#python tools/demo.py image -f exps/erosive_ulcer_mix/yolox_x_erosive_ulcer_mix.py -c YOLOX_outputs/yolox_x_erosive_ulcer_mix/best_ckpt.pth.tar --path ./datasets/gastric_object_detection/images/07e40950-e41d-4fb8-9a36-3dc15b63bb75.jpg --conf 0.2 --nms 0.1 --tsize 640 --save_result --device gpu
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/data2/qilei_chen/DATA/trans_drone/videos/whole_rounds/DJI_0601 Wide View 400.MOV" \
+    --tsize 960 --save_result --device gpu
 
