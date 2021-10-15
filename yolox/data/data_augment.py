4a745bcc1e1029277667a0ca4e03da7192370f08
qilei123 <qileimail123@gmail.com>
2021-10-15 01:51:01 -0400

    todo tests

--- yolox/data/data_augment.py
+++ yolox/data/data_augment.py
@@ -355,7 +355,7 @@ def random_perspective_(
         else:  # affine
 
             img = cv2.warpAffine(
-                img, R[:2], dsize=img.shape[1::-1], borderValue=(114, 114, 114)
+                img, M[:2], dsize=img.shape[1::-1], borderValue=(114, 114, 114)
             )
     print(targets)
     # Transform label coordinates
@@ -366,7 +366,7 @@ def random_perspective_(
         xy[:, :2] = targets[:, [0, 1, 2, 3, 0, 3, 2, 1]].reshape(
             n * 4, 2
         )  # x1y1, x2y2, x1y2, x2y1
-        xy = xy @ R.T  # transform
+        xy = xy @ M.T  # transform
         if perspective:
             xy = (xy[:, :2] / xy[:, 2:3]).reshape(n, 8)  # rescale
         else:  # affine
