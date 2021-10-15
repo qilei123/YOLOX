e5358c218cc7371087ddca5870619c02f3cf0280
qilei123 <qileimail123@gmail.com>
2021-10-15 02:18:21 -0400

    todo tests

--- aug_test.py
+++ aug_test.py
@@ -14,6 +14,6 @@ def rotate_image(image, angle):
 img = cv2.imread("/data1/qilei_chen/DEVELOPMENTS/YOLOX/datasets/gastric_object_detection/images/0_03_00.700000.jpg")
 
 
-res_img,_ = random_perspective(img,scale=[0.95,1.05],shear=2)
+res_img,_ = random_perspective(img,scale=[0.95,1.05],shear=0,translate=0.0)
 cv2.imwrite("/data2/qilei_chen/DATA/new_polyp_data_combination/testrp.jpg",res_img)
 #cv2.imwrite("/data2/qilei_chen/DATA/new_polyp_data_combination/test.jpg",rotate_image(img,25))
