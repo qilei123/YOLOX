aa3624fab01bd1255a8e6a7f18b5e9ba1353a527
qilei123 <qileimail123@gmail.com>
2021-10-21 19:30:27 -0400

    todo tests

--- demo.sh
+++ demo.sh
@@ -5,3 +5,62 @@ python tools/demo.py video \
     --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0081.MOV" \
     --tsize 960 --save_result --device gpu
 
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0080.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0079.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0078.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0071.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0068.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0067.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0066.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0065.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0064.MOV" \
+    --tsize 960 --save_result --device gpu
+
+python tools/demo.py video \
+    -f exps/trans_drone/yolox_x_trans_drone_mix_960.py \
+    -c YOLOX_outputs/yolox_x_trans_drone_mix_960/best_ap50_95_ckpt.pth \
+    --path "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0063.MOV" \
+    --tsize 960 --save_result --device gpu
