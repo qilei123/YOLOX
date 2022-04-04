5c7ca4156cbf2dca437048d16feeb2fb109f3f91
qilei123 <qileimail123@gmail.com>
2022-04-04 16:36:55 -0400

    test

--- exps/CAPSULEGI/yolox_s_640.py
+++ exps/CAPSULEGI/yolox_s_640.py
@@ -15,7 +15,7 @@ from yolox.exp import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
-        self.num_classes = 2
+        self.num_classes = 9
         self.depth = 0.33
         self.width = 0.50
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
